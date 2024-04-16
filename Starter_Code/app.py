85811cb662057c4832f9c29783a4d945a658c35f
JessH09 <jesshernan0919@gmail.com>
2024-04-15 21:27:54 -0400

    added api flask and updated code

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,29 +1,155 @@
 # Import the dependencies.
 
-
+import datetime as dt
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
-
-# reflect an existing database into a new model
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# reflect the database into a new model
+Base = automap_base()
 
 # reflect the tables
+Base.prepare(engine, reflect=True)
 
 
 # Save references to each table
-
+station = Base.classes.station
+measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
+app = Flask(__name__)
 
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def homepage():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"- List of prior year rain totals from all stations<br/>"
+        f"<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"- List of Station numbers and names<br/>"
+        f"<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"- List of prior year temperatures from all stations<br/>"
+        f"<br/>"
+        f"/api/v1.0/start<br/>"
+        f"- When given the start date format: (YYYY-MM-DD), calculates the MIN/AVG/MAX temperature for all dates greater than and equal to the start date<br/>"
+        f"<br/>"
+        f"/api/v1.0/start/end<br/>"
+        f"- When given the start and the end date format: (YYYY-MM-DD), calculate the MIN/AVG/MAX temperature for dates between the start and end date inclusive<br/>"
 
+    )
+#########################################################################################
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return a list of rain fall for prior year"""
+#    * Query for the dates and precipitation observations from the last year.
+#    * Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
+#    * Return the json representation of your dictionary.
+    last_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    rain = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date > last_year).\
+        order_by(measurement.date).all()
+
+# Create a list of dicts with `date` and `prcp` as the keys and values
+    rain_totals = []
+    for result in rain:
+        row = {}
+        row["date"] = result.date
+        row["prcp"] = result.prcp
+        rain_totals.append(row)
+
+    return jsonify(rain_totals)
+
+#########################################################################################
+@app.route("/api/v1.0/stations")
+def stations():
+    stations_query = session.query(station.name, station.station)
+    stations = pd.read_sql(stations_query.statement, stations_query.session.bind)
+    return jsonify(stations.to_dict())
+#########################################################################################
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list of temperatures for prior year"""
+#    * Query for the dates and temperature observations from the last year.
+#           * Convert the query results to a Dictionary using `date` as the key and `tobs` as the value.
+#           * Return the json representation of your dictionary.
+    last_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    temperature = session.query(measurement.date, measurement.tobs).\
+        filter(measurement.date > last_year).\
+        order_by(measurement.date).all()
+
+# Create a list of dicts with `date` and `tobs` as the keys and values
+    temperature_totals = []
+    for result in temperature:
+        row = {}
+        row["date"] = result.date
+        row["tobs"] = result.tobs
+        temperature_totals.append(row)
+
+    return jsonify(temperature_totals)
+#########################################################################################
+@app.route("/api/v1.0/<start>")
+def trip(start):
+    try:
+ # go back one year from start date and go to end of data for Min/Avg/Max temp   
+        start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    except ValueError:
+        return jsonify({"error": "Invalid date format. Please use YYYY-MM-DD format."}), 400
+ 
+    one_year_ago = start_date - dt.timedelta(days=365)
+    end_date =  dt.date(2017, 8, 23)
+    trip_data = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= one_year_ago).filter(measurement.date <= end_date).all()
+
+    trip_temps = list(np.ravel(trip_data))
+
+    return jsonify(trip_temps)
+
+#########################################################################################
+@app.route("/api/v1.0/<start>/<end>")
+def trip1(start,end):
+    try:
+        start_date = dt.datetime.strptime(start, '%Y-%m-%d')
+        end_date = dt.datetime.strptime(end, '%Y-%m-%d')
+    except ValueError:
+        return jsonify({"error": "Invalid date format. Please use YYYY-MM-DD format."}), 400
+
+    one_year_ago_start = start_date - dt.timedelta(days=365)
+    one_year_ago_end = end_date - dt.timedelta(days=365)
+
+    trip_data = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= one_year_ago_start).filter(measurement.date <= one_year_ago_end).all()
+
+    trip_temps = list(np.ravel(trip_data))
+
+    return jsonify(trip_temps)
+#########################################################################################
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 #################################################
 # Flask Routes
